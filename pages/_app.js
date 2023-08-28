cc6e9eb2f9b11c8a229421a03433dd6ef1b0fcbb
Tanveer H. <e.service.center1@gmail.com>
2023-08-28 18:42:41 +0530

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { appWithTranslation } from "next-i18next";
 import { AnimatePresence, motion } from "framer-motion";
+import Script from "next/script";
 import Layout from "@/components/Layout";
 import SliderTransition from "@/components/SliderTransition";
 import "@/styles/globals.css";
@@ -9,15 +10,33 @@ const MyApp = ({ Component, pageProps }) => {
   const router = useRouter();
 
   return (
-    
-    <Layout>
-      <AnimatePresence mode="wait">
-        <motion.div key={router.route} className="h-full">
-          <SliderTransition/>
-          <Component {...pageProps} />
-        </motion.div>
+    <>
+      <Script id="googleAnalyticsSRC"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="googleAnalyticsFunction"
+      strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
+      <Layout>
+        <AnimatePresence mode="wait">
+          <motion.div key={router.route} className="h-full">
+            <SliderTransition />
+            <Component {...pageProps} />
+          </motion.div>
         </AnimatePresence>
-    </Layout>
+      </Layout>
+    </>
   );
 };
 
