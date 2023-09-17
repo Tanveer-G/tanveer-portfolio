c376d106233cfe21753687f47d738276ea5822b5
Tanveer H. <e.service.center1@gmail.com>
2023-09-17 14:46:23 +0530

    Rename PDF of Resume

--- components/HomeButtons.jsx
+++ components/HomeButtons.jsx
@@ -1,27 +1,27 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
-import {AiOutlineDownload} from 'react-icons/ai'
+import { AiOutlineDownload } from "react-icons/ai";
 import style from "./css/HomeButtons.module.css";
-import Link from "next/link";
 
 const HomeButtons = () => {
   const { t } = useTranslation("home");
   const router = useRouter();
-  
+
   const downloadResumeBtn = () => {
-    const resumeUrl = "./TANVEER-Devloper-Resume.pdf";
+    const resumeUrl = "./Tanveer-Resume-Frontend-Developer.pdf";
     window.open(resumeUrl, "_blank");
   };
+
   return (
     <>
       <button className={style.hireBtn}>
-        <Link  href={`/${router.locale}/contact`}>
-          {t("hireMe")}
-        </Link>
+        <Link href={`/${router.locale}/contact`}>{t("hireMe")}</Link>
       </button>
 
       <button className={style.resumeBtn} onClick={() => downloadResumeBtn()}>
-        {t("resume")}<AiOutlineDownload className={style.iconDown}/>
+        {t("resume")}
+        <AiOutlineDownload className={style.iconDown} />
       </button>
     </>
   );
